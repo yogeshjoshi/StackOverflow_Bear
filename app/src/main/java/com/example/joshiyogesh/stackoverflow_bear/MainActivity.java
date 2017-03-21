e1ffe8cd6d3ab0391a9ce5b83174c17e8c6b6c58
yogeshjoshi <joshiyogesh001@gmail.com>
2017-03-21 20:38:26 +0530

    Xml file has been designed and objects corresponding to R , ids provided

--- app/src/main/java/com/example/joshiyogesh/stackoverflow_bear/MainActivity.java
+++ app/src/main/java/com/example/joshiyogesh/stackoverflow_bear/MainActivity.java
@@ -2,16 +2,34 @@ package com.example.joshiyogesh.stackoverflow_bear;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 /*
 * Created by @joshiYogesh on 21/03/2017 at 19:56 IST
 *
 * */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
 
+    SearchView mSearchView;
+    ListView questionList;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        questionList = (ListView)findViewById(R.id.QuestionList) ;
+        imageView = (ImageView)findViewById(R.id.introImage);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
