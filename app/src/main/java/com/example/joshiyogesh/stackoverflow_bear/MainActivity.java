03445a43630a2026fd43a9acb4f536faedbbfaa8
yogeshjoshi <joshiyogesh001@gmail.com>
2017-03-21 20:03:16 +0530

    Main Activity has been created || initial commit

---
+++ app/src/main/java/com/example/joshiyogesh/stackoverflow_bear/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.joshiyogesh.stackoverflow_bear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+* Created by @joshiYogesh on 21/03/2017 at 19:56 IST
+*
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
